ae576308ad056764e3e0d381a1ca1c540b3bd769
pythoninthegrass <lance.stephens@okstate.edu>
2015-09-17 19:08:45 -0400

    Finished 3.3

--- lance/ex3.3.py
+++ lance/ex3.3.py
@@ -4,21 +4,18 @@
 # D = >= 0.6
 # F = < 0.6
 
+grades = ["A", "B", "C", "D", "F"]
+# print "The range is 0.0 - 1.0" # trips up Coursera interpreter
 input = raw_input("What did you get on that Python test? ")
-score = float(input)
+score = float(input) #fails to conver integers (e.g., 90, 80, etc.)
 
 if score >= 0.9 :
-    print "A"
+    print grades[0]
 elif score >= 0.8 :
-    print "B"
+    print grades[1]
 elif score >= 0.7 :
-    print "C"
+    print grades[2]
 elif score >= 0.6 :
-    print "D"
-elif score < 0.6 :
-    try:
-        print "F"
-    except:
-        score = -1
-
-# print "At least you tried! "
+    print grades[3]
+elif score <= 0.6 :
+    print grades[4]
