67f71f174b4c9bbc46b45021f994432958dce48f
pythoninthegrass <lance.stephens@okstate.edu>
2015-09-18 16:52:03 -0400

    Added Ch. 3 Quiz Programs

--- scratch.py
+++ scratch.py
@@ -1,8 +1,48 @@
+
+
 # x = (42 % 10)
 # print x
 
 # x = 1 + 2 * 3 - 8 / 4
 # print x
 
-x = int(98.6)
-print x
+# x = int(98.6)
+# print x
+
+# x = 4
+# if  x == 5 :
+#     print 'Is 5'
+#     print 'Is Still 5'
+#     print 'Third 5'
+
+# x = 0
+# if x < 2 :
+#     print 'Small'
+# elif x < 10 :
+#     print 'Medium'
+# else :
+#     print 'LARGE'
+# print 'All done'
+
+# x = -2.0
+# if x < 2 :
+#     print 'Below 2'
+# elif x >= 2 :
+#      print 'Two or more'
+# else :
+#     print 'Something else'
+    
+# astr = 'Hello Bob'
+# istr = int(astr)
+# print 'First', istr
+# astr = '123'
+# istr = int(astr)
+# print 'Second', istr
+
+# astr = 'Hello Bob'
+# istr = 0
+# try:
+#     istr = int(astr)
+# except:
+#     istr = -1
+# print istr
