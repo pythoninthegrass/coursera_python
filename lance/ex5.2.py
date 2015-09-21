dab5e53b4d059ec561bb50e561e5edfe99b0de48
pythoninthegrass <lance.stephens@okstate.edu>
2015-09-21 16:48:02 -0400

    Invalid input and mismatch errors when run through online Python interpreter.

--- lance/ex5.2.py
+++ lance/ex5.2.py
@@ -3,8 +3,34 @@
 #Then print out the largest and smallest numbers
 #Insert error handling for non numbers
 
+# largest = None
+# for num in [4, 5, 7]:
+#     try:
+#         num = raw_input("Enter a number: ")
+#         if num == "done":
+#             break
+#         if largest is None or num > largest :
+#             largest = num
+#     except:
+#         print "Invalid input"
+
+# print "Maximum is", largest
+
+# smallest = None
+# for num2 in [4, 5, 7]:
+#     try:
+#         num2 = raw_input("Enter a new number: ")
+#         if num2 == "done":
+#             break
+#         if smallest is None or num2 < smallest:
+#             smallest = num2
+#     except:
+#         print "Invalid input"
+
+# print "Minimum is", smallest
+
 largest = None
-for num in [4, 5, 7]:
+while True:
     try:
         num = raw_input("Enter a number: ")
         if num == "done":
@@ -14,10 +40,10 @@ for num in [4, 5, 7]:
     except:
         print "Invalid input"
 
-print "Maximum is ", largest
+print "Maximum is", largest
 
 smallest = None
-for num2 in [4, 5, 7]:
+while True:
     try:
         num2 = raw_input("Enter a new number: ")
         if num2 == "done":
@@ -27,4 +53,4 @@ for num2 in [4, 5, 7]:
     except:
         print "Invalid input"
 
-print "Minimum is ", smallest
+print "Minimum is", smallest
