a270b58b3a8e37bebbebebbe92367da5455e3a1a
pythoninthegrass <lance.stephens@okstate.edu>
2015-09-29 14:59:03 -0400

    unable to float line_num then store in numlist

--- lance/ex7.2.py
+++ lance/ex7.2.py
@@ -2,25 +2,39 @@
 fname = raw_input("Enter file name: ")
 fh = open(fname)
 
-total = 0
-count = 0
-
+numlist = list()
 for line in fh:
+    line = line.strip()
     if not line.startswith("X-DSPAM-Confidence:") : continue
     # print line # displays whole line
     # print line # displays X-DSPAM-Confidence: 0...
     # print line[0:18] # displays X-DSPAM-Confidence:
     # print line[20:] # displays 0...
-    line_num = line[20:] # stores 0...
-    
-value = float(line_num)
-total = total + value
-count = count + 1
+    # line_num = line[20:] # stores 0...
+    line_num = line.split(":")
+    # print line_num[1]
+    value = float(line_num)
+    numlist.append(value)
 
-average = total / count
+## Second Average List
+# value = float(line_num)
+average = sum(numlist) / len(numlist)
 print "Average spam confidence:",average
 
+## Original Average List
+# total = 0
+# count = 0
 
+# email = line_num[1]
+# total = total + email
+# # total = total + value
+# count = count + 1
+
+# average = total / count
+# print "Average spam confidence:",average
+
+
+## Detritus
 # for var in fh:
 #     count = count + 1
 #     print count
