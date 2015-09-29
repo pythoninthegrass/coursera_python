85f14e42d4829f028d3f08009fa509a1f769bed6
pythoninthegrass <lance.stephens@okstate.edu>
2015-09-29 16:31:06 -0400

    Right results locally, outer indentation on Python playground

---
+++ lance/ex7.2_edit.py
@@ -0,0 +1,20 @@
+# open a files and look for this 
+# X-DSPAM-Confidence:    0.8475
+# count the values from XDspam lines and make value floats
+# find average of values
+# Use the file name mbox-short.txt as the file name
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+
+total = 0
+count = 0
+nums = []
+
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+    	line = line.strip()
+    	nums.append(line[20:])
+        new_nums = [float(i) for i in nums]
+
+average = sum(new_nums) / len(new_nums)
+print "Average spam confidence:",average
