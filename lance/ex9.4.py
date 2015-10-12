1423cacad9600d067c59618d7ae4986c3b45a42c
pythoninthegrass <lance.stephens@okstate.edu>
2015-10-12 14:32:05 -0400

    Ex. 9.4 - needs to exclude 'From: 27' from output.

--- lance/ex9.4.py
+++ lance/ex9.4.py
@@ -1,15 +1,43 @@
 fname = raw_input("Enter file: ")
-if len(fname) < 1 : name = "mbox-short.txt"
 fh = open(fname)
+# text = fh.read() # Causes empty "{}" 
 
 counts = dict()
 
-# sender
-
 for line in fh:
     if line.startswith("From:"):
-        line = line.strip()
-        words = line.split() # useless with for function below
-        email.append(line[6:])
+        words = line.split()
+        for word in words:
+            if word not in counts:
+                counts[word] = 1
+            else:
+                counts[word] += 1
 
-print counts        
+print counts
+
+## Shorthand get function that does the for loop above
+# for word in words:
+#     counts[word] = counts.get(word,0) + 1
+
+# print counts.items()
+
+
+## Video Solution - Needs to exclude "From: 27"
+maxval = None
+maxkee = None
+
+for kee,val in counts.items():
+    if maxval == None or maxval < val :
+        maxval = val
+        maxkee = kee
+    # print kee, val, maxval, maxkee
+
+print maxkee, maxval
+
+
+## Forums Solution - Needs to exclude "From: 27"
+# import operator
+
+# highkey=max(counts.items(), key=operator.itemgetter(1))[0]
+
+# print highkey, counts[highkey]
