639a977d059c7e94527903c30d64214c9b7423cd
pythoninthegrass <lance.stephens@okstate.edu>
2015-10-12 22:01:06 -0500

    Ex. 9.4 Re-Up

--- scratch.py
+++ scratch.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 stuff = dict()
 print stuff.get('candy',-1)
 
@@ -139,3 +140,143 @@ print stuff.get('candy',-1)
 # except:
 #     istr = -1
 # print istr
+=======
+friends = [ 'Joseph', 'Glenn', 'Sally' ]
+friends.sort()
+print friends[0]
+
+# t = [9, 41, 12, 3, 74, 15]
+# print t[2:4]
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print len(c)
+
+# fruit = 'Banana'
+# # fruit[0] = 'b'
+# print len(fruit)
+
+# friends = [ 'Joseph', 'Glenn', 'Sally' ]
+# print friends[2]
+
+# s = 'word'
+# print('The full string is: ', s)
+# n = len(s)
+# for i in range(n):
+#     print()
+#     print('i =', i)
+#     print('The letter at index i:', s[i])
+#     print('The part before index i (if any):', s[:i])
+#     print('The part before index i+2:', s[:i+2])
+
+#fhand = open('mbox-short.txt')
+#inp = fhand.read()
+
+#fhand = open('mbox.txt')
+#x = 0
+#for line in fhand:
+#       x = x + 1
+#print x
+
+# smallest_so_far = None
+# for the_num in [9, 41, 12, 3, 74, 15] :
+#   if the_num < smallest_so_far :
+#       smallest_so_far = the_num
+# print smallest_so_far
+
+# zork = 0
+# for thing in [9, 41, 12, 3, 74, 15] :
+#     zork = zork + thing
+# print 'After', zork
+
+# tot = 0 
+# for i in [5, 4, 3, 2, 1] :
+#     tot = tot + 1
+# print tot
+
+# n = 5
+# while n > 0 :
+#     print n
+# print 'All done'
+
+# def stuff():
+#     print 'Hello'
+#     return
+#     print 'World'
+
+# stuff()
+
+# def addtwo(a, b):
+#     added = a + b
+#     return a
+
+# x = addtwo(2, 7)
+# print x
+
+# def func(x) :
+#     print x
+
+# func(10)
+# func(20)
+
+# x = 10.5 * 40 + (10.5 * 1.5 * (45 - 40))
+# print x
+
+# def computepay(h,r):
+#     return h + (r * 1.5 * (h - 40))
+
+# h = 45
+# r = 10.5
+
+# pay = computepay(h,r)
+# print pay
+
+
+# x = (42 % 10)
+# print x
+
+# x = 1 + 2 * 3 - 8 / 4
+# print x
+
+# x = int(98.6)
+# print x
+
+# x = 4
+# if  x == 5 :
+#     print 'Is 5'
+#     print 'Is Still 5'
+#     print 'Third 5'
+
+# x = 0
+# if x < 2 :
+#     print 'Small'
+# elif x < 10 :
+#     print 'Medium'
+# else :
+#     print 'LARGE'
+# print 'All done'
+
+# x = -2.0
+# if x < 2 :
+#     print 'Below 2'
+# elif x >= 2 :
+#      print 'Two or more'
+# else :
+#     print 'Something else'
+    
+# astr = 'Hello Bob'
+# istr = int(astr)
+# print 'First', istr
+# astr = '123'
+# istr = int(astr)
+# print 'Second', istr
+
+# astr = 'Hello Bob'
+# istr = 0
+# try:
+#     istr = int(astr)
+# except:
+#     istr = -1
+# print istr
+>>>>>>> Stashed changes
