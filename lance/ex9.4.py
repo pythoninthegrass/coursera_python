26935b9de6fcb71a4017b24ce3a2ffaed92dc7ff
pythoninthegrass <lance.stephens@okstate.edu>
2015-10-12 22:02:45 -0500

    Revert "Ex. 9.4 Re-Up"
    This reverts commit 639a977d059c7e94527903c30d64214c9b7423cd.

--- lance/ex9.4.py
+++ lance/ex9.4.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 fname = raw_input("Enter file: ")
 fh = open(fname)
 
@@ -25,20 +24,3 @@ for kee,val in counts.items():
         maxkee = kee
 
 print maxkee, maxval
-=======
-fname = raw_input("Enter file: ")
-if len(fname) < 1 : name = "mbox-short.txt"
-fh = open(fname)
-
-counts = dict()
-
-# sender
-
-for line in fh:
-    if line.startswith("From:"):
-        line = line.strip()
-        words = line.split() # useless with for function below
-        email.append(line[6:])
-
-print counts        
->>>>>>> Stashed changes
