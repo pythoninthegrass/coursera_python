4c18012cb7d85f6952ed4b6326a7c22293f7f7eb
pythoninthegrass <lance.stephens@okstate.edu>
2015-10-17 18:58:44 -0500

    Ex. 10.2 - copied code from prior assn

--- lance/ex10.2.py
+++ lance/ex10.2.py
@@ -1,3 +1,26 @@
+#Figure out the distribution by hour of the day for each of the messages. 
+#You can pull the hour out from the 'From ' line by finding the time and 
+#then splitting the string a second time using a colon.
+
+#From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+#Once you have accumulated the counts for each hour, print out the 
+#counts, sorted by hour as shown below.
+
 name = raw_input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+
+counts = dict()
+
+for line in fh:
+    if line.startswith("From:"):
+        words = line.split()
+        words.remove("From:")
+        for word in words:
+            if word not in counts:
+                counts[word] = 1
+            else:
+                counts[word] += 1
+
+print counts
