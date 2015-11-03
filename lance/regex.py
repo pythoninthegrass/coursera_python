3238150348e2b4d788b783b9ffff33fe989ebcf2
pythoninthegrass <lance.stephens@okstate.edu>
2015-11-02 19:00:34 -0500

    Regular Expression - Solved by nested for loop

--- lance/regex.py
+++ lance/regex.py
@@ -8,12 +8,30 @@ nums = list()
 
 for line in fh:
     line = line.rstrip()
-    # y = re.findall(r'([\d.]*\d+)', line)
-    y = re.findall('([0-9]+)', line)
-    if len(y) != 1: continue
-    # num = float(y[0])
-    num = int(y[0])
-    nums.append(num)
+    y = re.findall(r'\d+', line)
+    for item in y:
+        try:
+            num = int(item)
+        except val_error:
+            pass
+        else:
+            nums.append(num)
 
-# print nums
 print sum(nums)
+
+## Original falty solution
+# for line in fh:
+#     line = line.rstrip()
+    # y = re.findall('([0-9]+)', line)
+    # if len(y) != 1: continue # original check for integers
+    # if len(y) > 0 : continue
+    # num = float(y[0])
+    # num = int(y[0])
+    # nums.append(num)
+# print sum(nums)
+
+## Print the hard way -- same results
+## Uncomment "sum = 0" on line 8
+# for number in nums:
+#     sum = sum + number
+# print sum
