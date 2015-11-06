d6040a1a0038ff082aef0d2eda93f9d9fbcefd6d
pythoninthegrass <pythoninthegrass@users.noreply.github.com>
2015-11-05 18:53:39 -0600

    Scraping HTML Data with BeautifulSoup

--- lance/hdata-bs.py
+++ lance/hdata-bs.py
@@ -1,5 +1,6 @@
 # Sample data: http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_42.html (Sum=2553)
 # Actual data:  http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_166742.html (Sum ends with 12)
+# Renamed commit to match assignment
 
 import urllib
 from BeautifulSoup import *
